8f2646f8882c84b94de5c3a818567f3bd0c757d6
Maxim Novikov <maximkanv@gmail.com>
2021-06-12 16:55:29 +0300

    change api host

--- src/app/utils/fetchUtils.js
+++ src/app/utils/fetchUtils.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const webApi = 'https://my-json-server.typicode.com/ziprion/my-pokedex-app';
-const localApi = 'http://localhost:3000';
+const webApi = 'https://my-json-server.typicode.com/ziprion/my-pokedex-app';
+// const localApi = 'http://localhost:3000';
 const limit = 20;
 
-export const getPokemonsByPage = (page) => axios.get(`${localApi}/pokemons?_page=${page}&_limit=${limit}`);
-export const fetchPokemonById = (id) => axios.get(`${localApi}/pokemons/${id}`);
+export const getPokemonsByPage = (page) => axios.get(`${webApi}/pokemons?_page=${page}&_limit=${limit}`);
+export const fetchPokemonById = (id) => axios.get(`${webApi}/pokemons/${id}`);
